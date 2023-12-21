b60b9b5cd037674103ea9d3c81a518d8594562f3
kunalgharate1993 <kunalgharate1993@gmail.com>
2023-12-21 16:12:31 +0530

    registration page

--- lib/setting_page/change_password/change_password.dart
+++ lib/setting_page/change_password/change_password.dart
@@ -16,65 +16,81 @@ class _ChangePasswordState extends State<ChangePassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Change Password"),
+      appBar: AppBar(
+        title: Text("Change Password"),
       ),
-
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
-            child: Column(
-              children: [
-                Column(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset("assets/images/logo_icon.svg",
+                      width: 100, height: 100),
+                  Text(
+                    "Welcome to",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: AppColors.primary, fontSize: 30),
+                  ),
+                  Text(
+                    "Jire Mali Samaj",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: AppColors.primary,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SvgPicture.asset("assets/images/logo_icon.svg",width: 100,height: 100),
-                    Text(
-                      "Welcome to",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: AppColors.primary,fontSize: 30),
-
-                    ),Text(
-                      "Jire Mali Samaj",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: AppColors.primary,fontSize: 30,fontWeight: FontWeight.bold ),
-
+                    CustomInputFormField(
+                      hintText: "Enter Old Password",
+                      validator: (value) {},
+                      icon: Icons.lock,
                     ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    CustomInputFormField(
+                      hintText: "Enter New Password",
+                      validator: (value) {},
+                      icon: Icons.lock,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    CustomInputFormField(
+                      hintText: "Confirm Password",
+                      validator: (value) {},
+                      icon: Icons.lock,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 80, vertical: 10),
+                      child: CustomButton(
+                          onPress: () {}, buttonText: "Reset Password"),
+                    )
                   ],
                 ),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CustomInputFormField(hintText: "Enter Old Password" , validator: (value){}, icon: Icons.lock, ),
-                      SizedBox(height: 15,),
-
-                      CustomInputFormField(hintText: "Enter New Password" , validator: (value){}, icon: Icons.lock, ),
-                      SizedBox(height: 15,),
-
-                      CustomInputFormField(hintText: "Confirm Password" , validator: (value){}, icon: Icons.lock, ),
-                      SizedBox(height: 15,),
-
-                      SizedBox(
-                        height: 40,
-                      ),
-                      Padding(
-                        padding:const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-                        child: CustomButton(onPress: () {}, buttonText: "Reset Password"),
-
-
-                      )
-                    ],
-                  ),
-                )
-              ],
-
-            ),
+              )
+            ],
           ),
         ),
-
+      ),
     );
   }
 }
